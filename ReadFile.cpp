1846d3b763bb5464e6a8eab1b90aa5319fcddd20
Leung Chun Fai <47177060+JoakQQ@users.noreply.github.com>
2019-04-09 13:06:37 +0800

    Update and rename test1.cpp to ReadFile.cpp

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -6,7 +5,7 @@
 
 using namespace std;
 
-const int MAX = 3;
+const int MAX = 100;
 
 void ReadFile(string a[], int b[], int c[]){
 	ifstream fin("item.txt");
@@ -18,7 +17,6 @@ void ReadFile(string a[], int b[], int c[]){
 		iss.str(line);
 		iss >> a[i];
 		iss >> x;
-		
 		b[i] = atoi (x.c_str());
 		iss >> x;
 		c[i] = atoi (x.c_str());
@@ -31,10 +29,5 @@ int main(){
 	string Goods[MAX];
 	int profitsGoods[MAX], timesGoods[MAX];
 	ReadFile(Goods, profitsGoods, timesGoods);
-	for (int i = 0; i < MAX ; i++){
-		cout << Goods[i] << ' ';
-		cout << profitsGoods[i] << ' ';
-		cout << timesGoods[i] << endl;
-	}
-
+	return 0;
 }
