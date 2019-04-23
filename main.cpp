3a83106c1c10fb998a530bec06da373829337a4e
Leung Chun Fai <47177060+JoakQQ@users.noreply.github.com>
2019-04-24 02:16:37 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,34 +2,11 @@
 #include <string>
 #include <ctime>
 #include "ReadFile.cpp"
+#include "Customer.h"
 
 using namespace std;
 
-const int MAXGOODS = 100;
 const int MAX = 100;
-
-class Customers{
-public:
-	string goods[MAXGOODS];
-	int numOfGoods[MAXGOODS];
-	
-private:
-	int length(string arr[]){
-		int i = 0;
-		while(arr[i] != '\0'){
-			i++;
-			if (i == MAXGOODS){
-				break;
-			}
-		}
-		return i;
-	}
-	void printclass(){
-		for (int i = 0; i < Customers.length(Customers.goods); i++){
-			cout << goods[i] << " : " << numOfGoods[i] << endl;
-		}
-	}
-};
 
 int calculateRegisters(time){
 	
