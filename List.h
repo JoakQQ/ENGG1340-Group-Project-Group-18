d13f953cc49ba1ec7d949d2cfbfc48b7aa7c7b32
Leung Chun Fai <47177060+JoakQQ@users.noreply.github.com>
2019-04-26 02:41:04 +0800

    Create List.h

---
+++ List.h
@@ -0,0 +1,32 @@
+#ifndef _CUSTOMERIZED_LIST_
+#define _CUSTOMERIZED_LIST_
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct string_node
+{
+	string info;
+	string_node *next;
+};
+
+struct int_node
+{
+	int info;
+	int_node *next;
+};
+
+void append_string_list(string_node *&head, string info);
+
+void append_int_list(int_node *&head, int info);
+
+void print_string_list(string_node *head);
+
+void print_int_list(int_node *head);
+
+string value_string_node(string_node *head, int count);
+
+int value_int_node(int_node *head, int count);
+
+#endif
