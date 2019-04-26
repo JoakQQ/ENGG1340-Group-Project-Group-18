c42451a2f8ae80d7ee98f9c0b29dd8a6b3c4e0ea
johnyoung1998 <49027515+johnyoung1998@users.noreply.github.com>
2019-04-26 18:37:28 +0800

    Create test.h

---
+++ test.h
@@ -0,0 +1,15 @@
+#ifndef _TEST_GRAUD_
+#define _TEST_GRAUD_
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include "Customer.h"
+#include "ReadFile.h"
+using namespace std;
+
+void test();
+
+#endif
