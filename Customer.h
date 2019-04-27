71bdeef3b5e02761d62f4e76f8d63daf654cf73b
Leung Chun Fai <47177060+JoakQQ@users.noreply.github.com>
2019-04-27 16:10:52 +0800

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,7 +1,6 @@
 #ifndef _CUSTOMER_CLASS_
 #define _CUSTOMER_CLASS_
 
-#include <iostream>
 #include <string>
 #include "List.h"
 using namespace std;
@@ -10,14 +9,21 @@ class Customer
 {
 public:
     string_node *Goods = NULL;
+
     int_node *num_Goods = NULL;
+
     int length();
-    void printCustomer();
+
     void append_Goods(string info);
+
     void append_num_Goods(int info);
+
     string value_Goods(int count);
+
     int value_num_Goods(int count);
+
     bool find_Goods(string target);
+
     int find_num_Goods(string target);
 };
 
