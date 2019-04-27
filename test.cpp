69087dbe6d61278a5cec33cddf751e9129e2f9d6
johnyoung1998 <49027515+johnyoung1998@users.noreply.github.com>
2019-04-27 17:47:02 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,48 +6,48 @@
 #include "Customer.h"
 #include "File_IO.h"
 using namespace std;
-
+const int MAXGOOD=100; 
 int main(){
-  string Goods[50];
-  int b[50],c[50],num;
-  Customer s[50];
+  string Goods[MAXGOOD];
+  int b[MAXGOOD],c[MAXGOOD],num,NO;
+  cin>> NO;
+  Customer *s = new Customer [NO];
   int x=0;
   ReadFile(Goods,b,c,num);
-  for(int i=0;i<50;++i){
+  for(int i=0;i<NO;++i){
     srand(time(0)+x);
-    int mAx= rand()%3;
+    int mAx= rand()%10;
     int n=0;
-    int p[3]={-1,-1,-1};
+    int p[10];
     while (n<=mAx){
         int ct=0;
-        ++x;
-        ++n;
         int a = rand()% num;
         int b = rand()% 10+1;
-        for(int j=0;j<3;++j){
+        for(int j=0;j<n;++j){
             if(p[j]==a){
                 ct=1;
             }
         }
         if(ct==0){
-            cout << "attend " << i+1 << endl;
             s[i].append_Goods(Goods[a]);
             s[i].append_num_Goods(b);
         }
         p[n]=a;
+        ++x;
+        ++n;
     }
   }
 
   ofstream fout;
   fout.open("testing.txt");
-  fout<< 50<< endl;
-  for(int i=0;i<50;++i){
+  fout<< NO<< endl;
+  for(int i=0;i<NO;++i){
       fout<< s[i].length()<< endl;
       for(int j=0;j<s[i].length();++j){
           fout<< s[i].value_Goods(j)<< " "<< s[i].value_num_Goods(j)<< endl;
       }
   }
+  delete []s;
   fout.close();
   return 0;
 }
-
