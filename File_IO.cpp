22a1a45ac5686d62c693e5d3bbf88e7df68ee194
Leung Chun Fai <47177060+JoakQQ@users.noreply.github.com>
2019-04-27 16:09:59 +0800

    Update File_IO.cpp

--- File_IO.cpp
+++ File_IO.cpp
@@ -18,3 +18,18 @@ void ReadFile(string a[], int b[], int c[], int &num){
 	fin.close();
 	num = i;
 }
+
+void WriteINFO(Customer **ARR, int length[], int ARR_x){
+	ofstream fout("log.txt");
+	for (int i = 0; i < ARR_x; i++){
+		fout << "----- Register " << i + 1 << " -----" << endl;
+		for (int j = 0; j < length[i]; j++){
+			fout << "----- Customer " << j + 1 << " -----" << endl;
+			for (int k = 0; k < ARR[i][j].length(); k++){
+				fout << ARR[i][j].value_Goods(k) << " : " << ARR[i][j].value_num_Goods(k) << endl;
+			}
+		}
+		fout << endl;
+	}
+	fout.close();
+}
